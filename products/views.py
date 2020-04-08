1e1a848537f38fa80d484eb59d923387d3c585f5
Mahin Malhotra <mahinmalhotra20@gmail.com>
2020-04-08 20:26:01 +0530

    done

---
+++ products/views.py
@@ -0,0 +1,72 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Product
+from django.utils import timezone
+
+# Create your views here.
+def home(request):
+    products= Product.objects
+    return render(request,'products/home.html',{'products':products})
+
+""" @login_required
+def create(request):
+    if request.method == 'POST':
+        if request.POST['title'] and request.POST['body'] and  request.POST['url'] and  request.FILES['icon'] and request.FILES['images']:
+            product = Product()
+            product.body = request.POST['body']
+            product.title = request.POST['title']
+            if request.POST['url'].startswith('http://') or  request.POST['url'].startswith('https://'):
+                product.url = request.POST['url']
+            else:
+                product.url = 'http://' + request.POST['url']
+            product.icon = request.FILES['icon']
+            product.image = request.FILES['images']
+            product.pub_date = timezone.datetime.now()
+            product.hunter = request.user
+            product.save()
+            return redirect('home')
+
+        else:
+            return render(request,'products/create.html',{'error':'All fields are required!'})
+
+            
+        return render(request,'products/create.html')   """  
+
+@login_required(login_url='/accounts/signup')
+def create(request):
+    if request.method == 'POST':
+        if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.FILES['icon'] and request.FILES['image']:
+            product = Product()
+            product.title = request.POST['title']
+            product.body = request.POST['body']
+            if request.POST['url'].startswith('http://') or request.POST['url'].startswith('https://'):
+                product.url = request.POST['url']
+            else:
+                product.url = 'http://' + request.POST['url']
+            product.icon = request.FILES['icon']
+            product.image = request.FILES['image']
+            product.pub_date = timezone.datetime.now()
+            product.hunter = request.user
+            product.save()
+            return redirect('/products/' + str(product.id))
+            #return redirect('home')
+        else:
+            return render(request, 'products/create.html',{'error':'All fields are required.'})
+    else:
+        return render(request, 'products/create.html')
+
+
+def detail(request,product_id):
+    product=get_object_or_404(Product,pk=product_id)
+    return render(request,'products/detail.html',{'product':product}) 
+
+@login_required(login_url='/accounts/signup')
+def upvote(request,product_id):
+    if request.method == 'POST':
+        product=get_object_or_404(Product,pk=product_id)
+        product.total_votes +=1
+        product.save()
+        return redirect('/products/' + str(product.id))
+
+
+
